ef736ad4a451d704e058f7d74e59e487788c3342
Dongdong Yu <yudongdong1127@gmail.com>
2019-04-17 14:16:44 +1000

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 app.use(flash());
 app.use(methodOverride('_method'));
 
