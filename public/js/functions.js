200b52c677432b9b5b93d5362588323b9070f831
Dongdong Yu <yudongdong1127@gmail.com>
2019-05-01 23:46:18 +1000

    style create image & responsive adjustment

---
+++ public/js/functions.js
@@ -0,0 +1,26 @@
+$(function() {
+  $(document).scroll(function() {
+    var $nav = $('.sticky-top');
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      $('#imagePreviewImage').attr('src', e.target.result);
+      // $('#imagePreviewImage').attr(
+      //   'background-image',
+      //   'url(' + e.target.result + ')'
+      // );
+      $('#imagePreviewImage').hide();
+      $('#imagePreviewImage').fadeIn(650);
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$('#imageUpload').change(function() {
+  readURL(this);
+});
