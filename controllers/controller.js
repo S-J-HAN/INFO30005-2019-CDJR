2063a63dc7a71f1c0071f929cfffd8f03952408a
Dongdong Yu <yudongdong1127@gmail.com>
2019-05-29 13:20:02 +1000

    update

--- controllers/controller.js
+++ controllers/controller.js
@@ -20,7 +20,8 @@ var createUser = function(req, res) {
     username: req.body.username,
     email: req.body.email,
     age: req.body.age,
-    city: req.body.city
+    city: req.body.city,
+    about: ''
   });
   User.register(newUser, req.body.password, function(err, user) {
     if (err) {
@@ -36,130 +37,129 @@ var createUser = function(req, res) {
 };
 
 // ---------Photo method--------------
-var createPhoto = function (req, res) {
-    var bucket = gcs.bucket('gs://zeta-verbena-238512.appspot.com');
-    const gcsname = `${Date.now()}-${req.files[0].originalname}`;
-    const file = bucket.file(gcsname);
-    const stream = file.createWriteStream({
-	    metadata: {
-	    contentType: req.files[0].mimetype
-            },
-	    resumable: false
-    });
-    stream.on('error', (err) => {
-      console.log("hi")
-        req.files[0].cloudStorageError = err;
-    });
+var createPhoto = function(req, res) {
+  var bucket = gcs.bucket('gs://zeta-verbena-238512.appspot.com');
+  const gcsname = `${Date.now()}-${req.files[0].originalname}`;
+  const file = bucket.file(gcsname);
+  const stream = file.createWriteStream({
+    metadata: {
+      contentType: req.files[0].mimetype
+    },
+    resumable: false
+  });
+  stream.on('error', err => {
+    console.log('hi');
+    req.files[0].cloudStorageError = err;
+  });
 
-    stream.on('finish', () => {
-	     return file.makePublic()
-        .then(() => {
-          // const client = new vision.ImageAnnotatorClient();
-          // const [result] = await client.labelDetection(`gs://${bucket.name}/${gcsame}`);
-          // const labels = result.labelAnnotations;
-          // var labelsFinal = []
-          // for (var i=0; i<labels.length; i++) {
-          //   if (labels[i].description.includes("paint")) {
-          //     if (labels[i].accuracy > 0.8) {
-          //       labelsFinal.push(labels[i].description)
-          //     }
-          //   } else {
-          //     labelsFinal.push(labels[i].description)
-          //   }
-          // }
+  stream.on('finish', () => {
+    return file
+      .makePublic()
+      .then(() => {
+        // const client = new vision.ImageAnnotatorClient();
+        // const [result] = await client.labelDetection(`gs://${bucket.name}/${gcsame}`);
+        // const labels = result.labelAnnotations;
+        // var labelsFinal = []
+        // for (var i=0; i<labels.length; i++) {
+        //   if (labels[i].description.includes("paint")) {
+        //     if (labels[i].accuracy > 0.8) {
+        //       labelsFinal.push(labels[i].description)
+        //     }
+        //   } else {
+        //     labelsFinal.push(labels[i].description)
+        //   }
+        // }
 
-    	    var imgurl = 'https://storage.googleapis.com/'+bucket.name+'/'+gcsname;
+        var imgurl =
+          'https://storage.googleapis.com/' + bucket.name + '/' + gcsname;
 
-    	    var newPhoto = new Photo({
-      	    "name": req.body.name,
-      	    "description": req.body.description,
-      	    "image": imgurl,
-      	    "postAt": req.body.date,
-      	    "author": {
-          		id: req.user._id,
-          		username: req.user.username
+        var newPhoto = new Photo({
+          name: req.body.name,
+          description: req.body.description,
+          image: imgurl,
+          postAt: req.body.date,
+          author: {
+            id: req.user._id,
+            username: req.user.username
+          }
+          // "labels": labelsFinal
+        });
+        newPhoto.save(function(err, newPhoto) {
+          if (!err) {
+            if (req.files.length == 1) {
+              res.redirect('/photo');
+            } else {
+              createChildPhoto(req.files.slice(1), newPhoto._id, res);
             }
-            // "labels": labelsFinal
-    	    });
-        	newPhoto.save(function (err, newPhoto) {
-        	    if (!err) {
-            		if (req.files.length==1) {
-          		    res.redirect('/photo')
-            		} else {
-          		    createChildPhoto(req.files.slice(1), newPhoto._id, res);
-            		}
-        	    } else {
-                console.log(err)
-      		      res.sendStatus(400);
-              }
-        	})
-	     })
-       .catch(err => {
-           console.error('ERROR:', err);
-           var newPhoto = new Photo({
-               name: req.body.name,
-               description: req.body.description,
-               image: req.body.image,
-               author: {
-                   id: req.user._id,
-                   username: req.user.username
-               }
-             });
-             newPhoto.save(function(err, newPhoto) {
-               if (!err) {
-                 res.redirect('/photo');
-               } else {
-                 res.sendStatus(400);
-               }
-             });
-       });
-    });
-    stream.end(req.files[0].buffer);
-}
+          } else {
+            console.log(err);
+            res.sendStatus(400);
+          }
+        });
+      })
+      .catch(err => {
+        console.error('ERROR:', err);
+        var newPhoto = new Photo({
+          name: req.body.name,
+          description: req.body.description,
+          image: req.body.image,
+          author: {
+            id: req.user._id,
+            username: req.user.username
+          }
+        });
+        newPhoto.save(function(err, newPhoto) {
+          if (!err) {
+            res.redirect('/photo');
+          } else {
+            res.sendStatus(400);
+          }
+        });
+      });
+  });
+  stream.end(req.files[0].buffer);
+};
 
 var createChildPhoto = function(childfilelist, parentfileID, res) {
-    var bucket = gcs.bucket('gs://zeta-verbena-238512.appspot.com');
-    const gcsname = `${Date.now()}-${childfilelist[0].originalname}`;
-    const file = bucket.file(gcsname);
-    const stream = file.createWriteStream({
-	    metadata: {
-	    contentType: childfilelist[0].mimetype
-            },
-	    resumable: false
+  var bucket = gcs.bucket('gs://zeta-verbena-238512.appspot.com');
+  const gcsname = `${Date.now()}-${childfilelist[0].originalname}`;
+  const file = bucket.file(gcsname);
+  const stream = file.createWriteStream({
+    metadata: {
+      contentType: childfilelist[0].mimetype
+    },
+    resumable: false
+  });
+  stream.on('error', err => {
+    childfilelist[0].cloudStorageError = err;
+  });
+
+  stream.on('finish', () => {
+    return file.makePublic().then(() => {
+      var imgurl =
+        'https://storage.googleapis.com/' + bucket.name + '/' + gcsname;
+      var newChildPhoto = new ChildPhoto({
+        image: imgurl,
+        parent: parentfileID
+      });
+      newChildPhoto.save(function(err, newChildPhoto) {
+        if (!err) {
+          if (childfilelist.length == 1) {
+            res.redirect('/photo');
+          } else {
+            createChildPhoto(childfilelist.slice(1), parentfileID, res);
+          }
+        } else {
+          res.sendStatus(400);
+        }
+      });
     });
-    stream.on('error', (err) => {
-        childfilelist[0].cloudStorageError = err;
-    });
+  });
 
-    stream.on('finish', () => {
-	return file.makePublic()
-        .then(() => {
-	    var imgurl = 'https://storage.googleapis.com/'+bucket.name+'/'+gcsname;
-	    var newChildPhoto = new ChildPhoto({
-	    "image": imgurl,
-	    "parent": parentfileID
-            });
-	newChildPhoto.save(function (err, newChildPhoto) {
-	    if (!err) {
-		if(childfilelist.length==1){
+  stream.end(childfilelist[0].buffer);
+};
 
-		    res.redirect('/photo')
-	        }
-		else{
-		    createChildPhoto(childfilelist.slice(1), parentfileID, res,);
-		}
-	    } else {
-		res.sendStatus(400);
-            }
-	});
-	});
-    });
-
-    stream.end(childfilelist[0].buffer);
-
-}
-
-var setLabel = async function (req, res) {
+var setLabel = async function(req, res) {
   try {
     const client = new vision.ImageAnnotatorClient();
     const [result] = await client.labelDetection(req.body.image);
@@ -196,25 +196,33 @@ var findAllPhotos = function(req, res) {
   });
 };
 
-var findOnePhoto = function(req, res){
-    var getImage = function(item){
-	    return item.image;
-    }
-    Photo.findById(req.params.id).populate('comments').exec(function(err, foundPhoto){
-        if(!err){
-	    ChildPhoto.find({'parent': req.params.id}).exec(function(err, foundSet){
-		if(!err){
-
-		    console.log(foundSet);
-		    console.log(foundSet.map(getImage));
-            	    res.render('photos/show', {photo: foundPhoto, moment: moment, children: foundSet.map(getImage)})
-		}else{
-		    res.sendStatus(404);
-		}
-	    });
-        }else{
+var findOnePhoto = function(req, res) {
+  var getImage = function(item) {
+    return item.image;
+  };
+  Photo.findById(req.params.id)
+    .populate('comments')
+    .exec(function(err, foundPhoto) {
+      if (!err) {
+        ChildPhoto.find({ parent: req.params.id }).exec(function(
+          err,
+          foundSet
+        ) {
+          if (!err) {
+            console.log(foundSet);
+            console.log(foundSet.map(getImage));
+            res.render('photos/show', {
+              photo: foundPhoto,
+              moment: moment,
+              children: foundSet.map(getImage)
+            });
+          } else {
             res.sendStatus(404);
-        }
+          }
+        });
+      } else {
+        res.sendStatus(404);
+      }
     });
 };
 
